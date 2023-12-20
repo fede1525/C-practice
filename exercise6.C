c723c51fd088e5909349015aeed511b746675481
Federico Resano <federicoresano1@gmail.com>
2023-12-20 13:41:07 -0300

    commit

--- exercise6.C
+++ exercise6.C
@@ -1,29 +1,16 @@
-// Write a C program to compute the perimeter and area of a circle with a height of 7 cm and width of 5 cm.
+// Write a C program to compute the perimeter and area of a circle with a given radius. 
 
 #include <stdio.h>
+#include <math.h>
 
-int calculatePerimeter(int radius, float PI);	
-int calculateArea(int radius, float PI);
+int main() {
+    double radius = 5.0;
+	double PI = 3.141593;
+    double perimeter = 2 * PI * radius;
+    double area = PI * pow(radius, 2);
 
-int main(){
+    printf("Circle Perimeter: %.2f cm\n", perimeter);
+    printf("Circle Area: %.2f square cm\n", area);
 
-	int height = 7;
-	int width = 5;
-	int radius = width/2;
-	const float PI = 3.141593;
-
-	printf("Perimeter: %d cm\n", calculatePerimeter(radius, PI));
-
-	printf("Area: %d cm\n", calculateArea(radius, PI));
-		
-	return 0;	
+    return 0;
 }
-
-
-	int calculatePerimeter(int radius, float PI){
-		return (2 * radius * PI);		
-	}
-
-	int calculateArea(int radius, float PI){
-		return PI * radius * radius;
-	}
