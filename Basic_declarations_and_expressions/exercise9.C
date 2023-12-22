7f57f73abff16a4752604772c11597b2011a1afc
Federico Resano <federicoresano1@gmail.com>
2023-12-22 19:51:36 -0300

    commit

---
+++ Basic_declarations_and_expressions/exercise9.C
@@ -0,0 +1,19 @@
+// Write a C program that accepts two integers from the user and calculates the sum of the two integers.
+
+#include <stdio.h>
+
+int main(){
+
+	int num1, num2;
+
+	printf("Enter the first number: \n");
+	scanf("%d", &num1);
+
+	printf("Enter the second number: \n");
+	scanf("%d", &num2);
+
+	int result = num1 + num2;
+	printf("Result: %d", result);
+
+	return 0;
+}
